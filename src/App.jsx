0b756c69903663c8946b998c5d9c938ddd4fa3d0
AsheshRanaGurung <ashesh7272@gmail.com>
2023-09-10 06:56:37 +0545

    feat: new data

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,26 @@ import { useEffect, useState } from "react";
 import RecipeCard from "./components/RecipeCard";
 
 const App = () => {
-    const[recipe,setRecipe]=useState("chicken")
-    const[recipeList,setRecipeList]=useState([])
-    const baseURL="https://api.edamam.com";
-  
+    const [recipe, setRecipe] = useState("chicken")
+    const [recipeList, setRecipeList] = useState([])
+    const baseURL = "https://api.edamam.com";
 
- const   APP_ID = "e119e0c8";
-   const  APP_KEY = "f5bbc911201ddaa8b732d93b5c886884";
-useEffect(()=>{
+    // const APP_ID = import.meta.env.VITE_APP_ID;
+    // const APP_KEY = import.meta.env.VITE_APP_KEY;
 
-    axios.get(`${baseURL}/search?q=${recipe}&app_id=${APP_ID}&app_key=${APP_KEY}`).then((response)=>{
-        setRecipeList(response.data.hits)
-    }).catch(()=>{})
-},[])
+    const APP_ID = "e119e0c8";
+    const APP_KEY = "f5bbc911201ddaa8b732d93b5c886884";
 
-  return (
- <RecipeCard recipeList={recipeList}/>
-  );
+    useEffect(() => {
+
+        axios.get(`${baseURL}/search?q=${recipe}&app_id=${APP_ID}&app_key=${APP_KEY}`).then((response) => {
+            setRecipeList(response.data.hits)
+        }).catch(() => { })
+    }, [])
+
+    return (
+        <RecipeCard recipeList={recipeList} />
+    );
 };
 
 export default App;
--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,42 +1,35 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
+import React from "react";
 import style from "../recipe.module.css";
-import {Box, Grid, Image, Text} from "@chakra-ui/react"
+import { Box, Grid, Image, Text } from "@chakra-ui/react";
 import { Spinner } from "@chakra-ui/spinner";
-const RecipeCard = ({recipeList}) => {
-  return (
-    <>
-    {recipeList.length ===0?
-<Box  display="flex"  justifyContent={"center"} width="100%">
-<Spinner
-
-  size='xl'
-/>
-</Box>
-    :
-    <Grid templateColumns='repeat(2, 1fr)' gap={6}>
-        {recipeList.map((food,index)=>{
-            return(
-              <>
-                <div key={index} className={style.recipe}>
-                    
-                <Text>
-                {food.recipe.label}
-    
-                </Text> 
-                <Image className={style.image} alt="food image" src={food.recipe.image}/>
-                    
-                    </div>
-              </>
-            )
-
-        })
-       }
-        
-    </Grid>
-}
+const RecipeCard = ({ recipeList }) => {
+    return (
+        <>
+            {recipeList.length === 0 ? (
+                <Box display="flex" justifyContent={"center"} width="100%">
+                    <Spinner />
+                </Box>
+            ) : (
+                <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+                    {recipeList.map((food, index) => {
+                        return (
+                            <>
+                                <div key={index} className={style.recipe}>
+                                    <Text>{food.recipe.label}</Text>
+                                    <Image
+                                        className={style.image}
+                                        alt="food image"
+                                        src={food.recipe.image}
+                                    />
+                                </div>
+                            </>
+                        );
+                    })}
+                </Grid>
+            )}
         </>
-   
-  )
-}
+    );
+};
 
-export default RecipeCard
+export default RecipeCard;
